918f7d6fc0fb18839f848209194b95c78ec946f4
Frank Boyer <frankboyer81@gmail.com>
2025-07-21 06:38:09 -0600

    Update calculadora_riesgos.py

--- calculadora_riesgos.py
+++ calculadora_riesgos.py
@@ -1,6 +1,5 @@
 # Configuración de la página de Streamlit para usar un diseño amplio
 st.set_page_config(layout="wide")
-
 # --- CSS personalizado ---
 # Se define un estilo CSS personalizado para mejorar la apariencia de los botones y selectores
 st.markdown(
